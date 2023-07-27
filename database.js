5b47fb177b9295a5a60215e1b54e3df398a63eaa
PhongDiep <phongdiep0712@gmail.com>
2023-07-27 09:30:00 -0700

    Create a free mysql on online and add the post route

--- database.js
+++ database.js
@@ -17,13 +17,19 @@ module.exports.getUsers = async function getUsers() {
   const users = await pool.query("Select * from users")
   return users
 }
-module.exports.createUser =  async function createUser(name, email, password)
+module.exports.createUser =  async function createUser(id, name, email, password)
 {
-  const user = await pool.query(`INSERT INTO users VALUES (?, ?, ?);`, [name, email, password])
+  const user = await pool.query(`INSERT INTO users VALUES (?, ?, ?, ?);`, [id, name, email, password])
   return user
 }
 module.exports.getUserByName =  async function getUserByName(name) 
 {
   const user = await pool.query(`Select * from users where name = ?`, [name])
   return user[0][0]
+}
+
+module.exports.setup = async function setup(table)
+{
+  const createdtable = await pool.query(`Create table if not exists ${table} (id Int auto_increment primary key, name Varchar(50) not null, email Varchar(50) not null, password varchar(50) not null)`)
+  console.log(`Created table ${table}`)
 }
--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const database = require('./database.js')
 
+//database.setup('users')
+
+app.use(express.json())
+
 app.get("/users", async (req, res) => {
   const users = await database.getUsers()
   res.send(users[0])
 })
+
+app.post("/users", async (req, res) => {
+  console.log(req)
+  const user = await database.createUser(req.body.id, req.body.name, req.body.email, req.body.password)
+  res.send(user)
+})
+
 
 app.use((err, req, res, next) => {
   console.log(err.stack)
